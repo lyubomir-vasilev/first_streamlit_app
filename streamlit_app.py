df4c27018e05addf067d811e48315206523b1aac
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 16:56:12 +0100

    Update streamlit_app.py
    changed indentetion in the try

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,12 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized 
 
+#New section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!')
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-      streamlit.error("Please select a fruit to get information") 
+    streamlit.error("Please select a fruit to get information") 
   else: 
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function) 
@@ -50,15 +51,19 @@ except URLError as e:
 
 
 streamlit.header("The fruit load list contains:")
+#snowflake related function 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
+#Adda button to load the fruit 
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-#
+
+
+
 streamlit.stop()
 #allow the user to add a fruit 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?", 'Jackfruit')
