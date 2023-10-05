6b0fe7f3c1682c23630214618177f839f3f38b66
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 16:34:12 +0100

    Update streamlit_app.py
    added the funciton in and adjusted the code after try:

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header('Fruityvice Fruit Advice!')
+#create a function 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityfice_normalized 
 
+streamlit.header('Fruityvice Fruit Advice!')
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information") 
   else: 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # Normalise the json response by turning it into a dataframe 
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    #display the dataframe created above on the app 
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(back_from_fruit)
+    streamlit.dataframe(back_from_function) 
+    
 except URLError as e:
   streamlit.error()
 
