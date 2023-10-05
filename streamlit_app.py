0fd5806166993a0c63a4ac98e4cb60393abccc0c
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 13:59:10 +0100

    Update streamlit_app.py
    forgot to change pandas to pd again

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # Normalise the json response by turning it into a dataframe 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #display the dataframe created above on the app 
 streamlit.dataframe(fruityvice_normalized)
