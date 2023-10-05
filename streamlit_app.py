0e5be2e69a677b8820d07717e27503656555a8cb
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 15:37:25 +0100

    Update streamlit_app.py
    changed the + with a , when using streamlit.write

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 #allow the user to add a fruit 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?, 'Jackfruit')
-streamlit.write('Thanks for adding' + add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
