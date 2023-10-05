2df064774d9c809eb064fa059959d0250a47cbbf
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 14:01:59 +0100

    Update streamlit_app.py
    think the + was not necessary

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 # Normalise the json response by turning it into a dataframe 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
