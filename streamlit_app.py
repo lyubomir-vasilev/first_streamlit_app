384257acc5bb69acd3f7c0b3ac233fca77d356e3
lyubomir-vasilev <130079626+lyubomir-vasilev@users.noreply.github.com>
2023-10-05 14:01:22 +0100

    Update streamlit_app.py
    removed the text displaying the raw json 
    then passed +kiwi into the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+kiwi")
 
 # Normalise the json response by turning it into a dataframe 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
